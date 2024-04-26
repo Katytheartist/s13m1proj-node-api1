4e03e42cab4094ded734adca763afbfab11bc1fa
Katy Kane <katytheartist@gmail.com>
2024-04-25 23:10:04 -0500

    wip1

--- api/server.js
+++ api/server.js
@@ -1,3 +1,28 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res)=>{
+    User.find()
+    .then(users=>{
+        //throw new Error('ahhhhh!!!!!')
+        //console.log(users)
+        res.json(users)
+    })
+    .catch(err=>{
+        res.status(500).json({
+            message: 'error gettin users',
+            err: err.message, 
+            stack: err.stack
+        })
+    })
+})
+
+server.use('*', (req, res)=>{
+    res.status(404).json({
+        message: 'losiento'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
